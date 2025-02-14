76310e31c2e81ed196deb1f53b14e767cc181015
THIS IS MY NAME <github@0x3d.uk>
2025-02-14 23:34:00 +0000

    fuck it

--- build.sh
+++ build.sh
@@ -11,18 +11,6 @@ echo "Installing Chromium..."
 rpm-ostree install chromium \
     libappindicator-gtk3 gnome-shell-extension-appindicator gnome-extensions-app
 
-# Download & set up AppImageLauncher (instead of RPM)
-echo "Installing AppImageLauncher..."
-mkdir /usr/local/bin/AppImageLauncher
-curl -L -o /usr/local/bin/AppImageLauncher "https://github.com/TheAssassin/AppImageLauncher/releases/download/v2.2.0/appimagelauncher-lite-2.2.0-travis995-0f91801-x86_64.AppImage"
-chmod +x /usr/local/bin/appimagelauncher-lite-2.2.0-travis995-0f91801-x86_64.AppImage
-
-# Download & set up OrcaSlicer AppImage
-echo "Installing OrcaSlicer..."
-mkdir /usr/local/bin/OrcaSlicer
-curl -L -o /usr/local/bin/OrcaSlicer "https://github.com/SoftFever/OrcaSlicer/releases/download/nightly-builds/OrcaSlicer_Linux_AppImage_V2.3.0-dev.AppImage"
-chmod +x /usr/local/bin/OrcaSlicer_Linux_AppImage_V2.3.0-dev.AppImage
-
 echo "Custom Kinoite build complete!"
 
 systemctl enable podman.socket
