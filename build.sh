1ba5dbc9e042e425b553aa0705aa97bad2faadc4
THIS IS MY NAME <github@0x3d.uk>
2025-02-14 23:27:43 +0000

    Update build.sh

--- build.sh
+++ build.sh
@@ -13,11 +13,13 @@ rpm-ostree install chromium \
 
 # Download & set up AppImageLauncher (instead of RPM)
 echo "Installing AppImageLauncher..."
+mkdir /usr/local/bin/AppImageLauncher
 curl -L -o /usr/local/bin/AppImageLauncher "https://github.com/TheAssassin/AppImageLauncher/releases/download/v2.2.0/appimagelauncher-lite-2.2.0-travis995-0f91801-x86_64.AppImage"
 chmod +x /usr/local/bin/appimagelauncher-lite-2.2.0-travis995-0f91801-x86_64.AppImage
 
 # Download & set up OrcaSlicer AppImage
 echo "Installing OrcaSlicer..."
+mkdir /usr/local/bin/OrcaSlicer
 curl -L -o /usr/local/bin/OrcaSlicer "https://github.com/SoftFever/OrcaSlicer/releases/download/nightly-builds/OrcaSlicer_Linux_AppImage_V2.3.0-dev.AppImage"
 chmod +x /usr/local/bin/OrcaSlicer_Linux_AppImage_V2.3.0-dev.AppImage
 
